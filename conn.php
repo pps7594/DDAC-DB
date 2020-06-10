035963060804e08177ae037de57d7ee71ce4acb6
pps7594 <58364431+pps7594@users.noreply.github.com>
2020-06-10 13:56:20 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -3,17 +3,17 @@
   </head>
   <body>
 <?php
-    ini_set('display_errors', 1);
-$connectionInfo = array("UID" => "apuadmin", "pwd" => "apu@1234", "Database" => "DDACDBTP042049", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:dbservertp042049.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+echo 'hi';
+// $connectionInfo = array("UID" => "apuadmin", "pwd" => "apu@1234", "Database" => "DDACDBTP042049", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+// $serverName = "tcp:dbservertp042049.database.windows.net,1433";
+// $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-if(!$conn){
-  echo "Error"
-}
-else{
-  echo "Good"
-}
+// if(!$conn){
+//   echo "Error"
+// }
+// else{
+//   echo "Good"
+// }
 ?>
   </body>
 </html>
