c59deac850cb12949a38a64e02578ebd61382fad
pps7594 <58364431+pps7594@users.noreply.github.com>
2020-06-10 14:17:36 +0800

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,11 +1,11 @@
 <?php
-$connectionInfo = array("UID" => "apuadmin", "pwd" => "apu@1234", "Database" => "WebAppDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:tp042049testdb.database.windows.net,1433";
+$connectionInfo = array("UID" => "apuadmin", "pwd" => "apu@1234", "Database" => "DDACDBTP042049", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:dbservertp042049.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-
-if(!$conn){
-  die("Error Connect :".sqlsrv_errors();
-}
+if(!$conn)
+  {
+    die("Error connection: ".print_r( sqlsrv_errors()));
+  }
 $sql = "INSERT INTO restaurant (restaurant_name, restaurant_address, restaurant_phone) VALUES (?, ?, ?)"; $params = array("Burger King", "Bukit Jalil", "03-11223444"); 
 $stmt = sqlsrv_query( $conn, $sql, $params); 
 if( $stmt === false ) { die( print_r( sqlsrv_errors(), true)); } 
