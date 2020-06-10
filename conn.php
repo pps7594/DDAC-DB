63935010cef2a14c8ceafe6f1e37038966aa38b4
pps7594 <58364431+pps7594@users.noreply.github.com>
2020-06-10 14:01:31 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -3,16 +3,14 @@
   </head>
   <body>
 <?php
-echo 'hi';
 $connectionInfo = array("UID" => "apuadmin", "pwd" => "apu@1234", "Database" => "DDACDBTP042049", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:dbservertp042049.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-// if(!$conn){
-//   echo "Error"
-// }
-// else{
-//   echo "Good"
-// }
+if(!$conn)
+  {
+    die("Error connection: ".sqlsrv_errors());
+  }
+echo "Connection to Db: Success!";
 ?>
   </body>
 </html>
