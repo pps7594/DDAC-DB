1a28b754a743df132086bf8c275c95132d47e932
pps7594 <58364431+pps7594@users.noreply.github.com>
2020-06-10 12:19:42 +0800

    Create restaurant.php

---
+++ restaurant.php
@@ -0,0 +1,28 @@
+<?php
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "pps7594", "pwd" => "{Google7594220}", "Database" => "WebAppDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:tp042049testdb.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if(!$conn){
+  die("Error Connect :".sqlsrv_errors();
+}
+
+$tsql= "SELECT * FROM [dbo].[restaurant]";
+$getResults= sqlsrv_query($conn, $tsql);
+if ($getResults == FALSE)
+{
+die(sqlsrv_errors());
+}
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+{
+echo "<tr>";
+echo "<td>". $row['restaurant_id'] . "</td>";
+echo "<td>". $row['restaurant_name'] ."</td>";
+echo "<td>". $row['restaurant_address'] . "</td>";
+echo "<td>". $row['restaurant_phone'] . "</td>";
+echo "</tr>";
+}
+sqlsrv_free_stmt($getResults);
+?>
